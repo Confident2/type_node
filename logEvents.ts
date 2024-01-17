76eed9c8f621b5a7450028665e008b97c8a7d055
mike <46815141+Confident2@users.noreply.github.com>
2024-01-17 09:59:51 +0100

    event log emitter

--- logEvents.ts
+++ logEvents.ts
@@ -1,8 +1,27 @@
 import { format } from "date-fns";
 import { v4 as uuid } from "uuid";
+import fs from "fs";
+import { promises as fsPromises, existsSync } from "fs";
+import path from "path";
 
-const formattedDate = format(new Date(), "yyyy-MM-dd\tHH:mm:ss");
 const generatedUuid = uuid();
 
-console.log(formattedDate);
-console.log(generatedUuid);
+const logEvents = async (message: string) => {
+  const formattedDate = `${format(new Date(), "yyyy-MM-dd\tHH:mm:ss")}`;
+  const logItem = ` ${formattedDate}\t${generatedUuid}\t${message}\n`;
+
+  console.log(logItem);
+  try {
+    if (!existsSync(path.join(__dirname, "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", "eventLog.txt"),
+      logItem
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default logEvents;
