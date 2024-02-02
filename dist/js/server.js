9f23dcd6e8ce3592f7f1512a82083768f39a80f9
mike <46815141+Confident2@users.noreply.github.com>
2024-02-02 17:25:10 +0100

    web-server with built in modules

--- dist/js/server.js
+++ dist/js/server.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
@@ -10,30 +12,34 @@ const PORT = process.env.PORT || 3500;
 // serve static files
 app.use(express_1.default.static(path_1.default.join(__dirname, "/public")));
 app.get("^/$|/index(.html)?", (req, res) => {
-    //res.sendFile("./views/index.html", { root: __dirname });
-    res.sendFile(path_1.default.join(__dirname, "views", "index.html"));
+  //res.sendFile("./views/index.html", { root: __dirname });
+  res.sendFile(path_1.default.join(__dirname, "views", "index.html"));
 });
 app.get("/new-page(.html)?", (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "views", "new-page.html"));
+  res.sendFile(path_1.default.join(__dirname, "views", "new-page.html"));
 });
 // route handlers
-app.get("/hello(.html)?", (req, res, next) => {
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
     console.log("attempted");
     next();
-}, (req, res) => {
+  },
+  (req, res) => {
     res.send("hello");
-});
+  }
+);
 const one = (req, res, next) => {
-    console.log("one");
-    next();
+  console.log("one");
+  next();
 };
 const two = (req, res, next) => {
-    console.log("two");
-    next();
+  console.log("two");
+  next();
 };
 const three = (req, res, next) => {
-    console.log("three");
-    res.send("finished");
+  console.log("three");
+  res.send("finished");
 };
 app.get("/chain(.html)?", [one, two, three]);
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
