dea971d06662f536cdf6486188e5fc072acb40f5
confident2 <mykel.anigboya@yahoo.com>
2024-02-21 14:50:45 -0800

    ts express

--- server.ts
+++ server.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import { Request, Response, NextFunction } from "express";
-const app = express();
 import path from "path";
+import http from "http";
+import fs from "fs";
+import dotenv from "dotenv";
+import logEvents from "./logEvents";
+
+dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 3500;
 
-// serve static files
+// Serve static files
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("^/$|/index(.html)?", (req, res) => {
-  //res.sendFile("./views/index.html", { root: __dirname });
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
@@ -16,7 +22,7 @@ app.get("/new-page(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 
-// route handlers
+// Route handlers
 app.get(
   "/hello(.html)?",
   (req: Request, res: Response, next: NextFunction) => {
@@ -27,6 +33,7 @@ app.get(
     res.send("hello");
   }
 );
+
 const one = (req: Request, res: Response, next: NextFunction) => {
   console.log("one");
   next();
@@ -41,6 +48,7 @@ const three = (req: Request, res: Response, next: NextFunction) => {
   console.log("three");
   res.send("finished");
 };
+
 app.get("/chain(.html)?", [one, two, three]);
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
